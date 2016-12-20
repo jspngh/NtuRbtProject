a327db79a23d1474ba44614780cc128a4525661c
vdutor <dutordoirv@gmail.com>
2016-12-20 13:21:54 +0800

    shell script to control blocking of the speech recognition thread

--- speech/speech2text.py
+++ speech/speech2text.py
@@ -1,9 +1,16 @@
 import speech_recognition as sr
 from difflib import SequenceMatcher
+import time
+
+
+def heartbeat():
+    with open('last_callback.txt', 'w') as f:
+        f.write(time.strftime("%-S", time.localtime()) + "\n")
 
 def find_best_match(phrase, sentences):
     similarities = [similarity(phrase, s) for s in sentences]
-    return sentences[similarities.index(max(similarities))]
+    max_similarity = max(similarities)
+    return sentences[similarities.index(max_similarity)], max_similarity
 
 def read_sentences(file_name):
     return [line.rstrip('\n') for line in open(file_name)]
@@ -12,18 +19,25 @@ def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def phrase_callback(model, audio):
+    heartbeat()
     try:
-        phrase = model.recognize_sphinx(audio)
-        best_match = find_best_match(phrase, sentences)
+        phrase = model.recognize_google(audio)
+        best_match, score = find_best_match(phrase, sentences)
+        if score < 0.6:
+            best_match = ""
+
+        print "score:           ", score
         print "your sentence:   ", phrase
         print "best match:      ", best_match
-    except LookupError:
-        print "Could not understand audio"
+    except:
+        pass
 
 def main():
+    heartbeat()
     model = sr.Recognizer()
     source = sr.Microphone()
-    model.listen_in_background(source, phrase_callback)
+    model.operation_timeout = 5
+    model.listen_in_background(source, phrase_callback, phrase_time_limit = 4)
 
     while True:
         try:
